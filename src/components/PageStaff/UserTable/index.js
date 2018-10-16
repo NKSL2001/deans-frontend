b033668de2ce6f351a17c9c759a5baacb5920c2f
Mingyu Liu <m325liu@uwaterloo.ca>
2018-10-16 23:16:29 +0800

    User Center

---
+++ src/components/PageStaff/UserTable/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import { Modal, Button, Table } from "antd";
+import * as styles from "./style.scss";
+
+const deleteUser = () => {
+  Modal.confirm({
+    title: "Delete user?",
+    content: "This action is invertible",
+    onOk() {
+      console.log("Confirmed");
+    },
+    onCancel() {
+      console.log("Cancel");
+    }
+  });
+};
+
+const dataSource = [
+  {
+    key: "1",
+    username: "admin",
+    adminStatus: "Yes"
+  },
+  {
+    key: "2",
+    username: "callcenteroperator",
+    adminStatus: "No"
+  }
+];
+
+const UserTable = () => {
+  const COLUMNS = [
+    {
+      title: "Username",
+      dataIndex: "username",
+      key: "username"
+    },
+    {
+      title: "Admin Status",
+      dataIndex: "adminStatus",
+      key: "adminStatus"
+    },
+    {
+      title: "Action",
+      dataIndex: "action",
+      key: "action",
+      render: () => (
+        <div className={styles.actions}>
+          <Button onClick={deleteUser}>Edit</Button>
+          <Button onClick={deleteUser} type="danger">
+            Delete
+          </Button>
+        </div>
+      )
+    }
+  ];
+  return <Table dataSource={dataSource} columns={COLUMNS} />;
+};
+
+// UserTable.propTypes = {
+//   editCrisis: PropTypes.func.isRequired,
+//   dispatchCrisis: PropTypes.func.isRequired
+// };
+
+export default UserTable;
--- src/containers/PageStaff/PageUserCenter/index.js
+++ src/containers/PageStaff/PageUserCenter/index.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Button } from "antd";
+import UserTable from "@components/PageStaff/UserTable";
+import * as styles from "./style.scss";
 
 class PageUserCenter extends React.Component {
   render() {
     return (
       <div>
         <h1>User Center</h1>
-        <p>Hello, this is your user center!</p>
+        <div className={styles.subHeader}>
+          <div className={styles.item}>User List</div>
+          <div className={styles.item}>
+            <Button type="primary">Add user</Button>
+          </div>
+        </div>
+        <div className={styles.userTable}>
+          <UserTable />
+        </div>
       </div>
     );
   }
